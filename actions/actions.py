98ea9ccede7f8905bfbd7ff113e811b9de28938a
vân <01658635754levan@gmail.com>
2022-08-04 14:58:20 +0700

    add price action

--- actions/actions.py
+++ actions/actions.py
@@ -13,22 +13,22 @@ from typing import Any, Text, Dict, List
 from html import entities
 
 from sqlalchemy import true
-product = [{"id": "mk1", "name": "áo trái tim", "color": "xanh", "stock": 0},
-           {"id": "mk1", "name": "áo trái tim", "color": "đỏ", "stock": 2},
-           {"id": "mk1", "name": "áo trái tim", "color": "tím", "stock": 2},
-           {"id": "mk1", "name": "áo trái tim", "color": "vàng", "stock": 2},
-           {"id": "mk2", "name": "áo cô gái", "color": "xanh", "stock": 2},
-           {"id": "mk2", "name": "áo cô gái", "color": "đỏ", "stock": 2},
-           {"id": "mk2", "name": "áo cô gái", "color": "tím", "stock": 2},
-           {"id": "mk2", "name": "áo cô gái", "color": "vàng", "stock": 2},
-           {"id": "mk3", "name": "áo bọt biển", "color": "xanh", "stock": 2},
-           {"id": "mk3", "name": "áo bọt biển", "color": "đỏ", "stock": 0},
-           {"id": "mk3", "name": "áo bọt biển", "color": "tím", "stock": 0},
-           {"id": "mk3", "name": "áo bọt biển", "color": "vàng", "stock": 0},
-           {"id": "mk4", "name": "áo cây dừa", "color": "xanh", "stock": 9},
-           {"id": "mk4", "name": "áo cây dừa", "color": "đỏ", "stock": 7},
-           {"id": "mk4", "name": "áo cây dừa", "color": "tím", "stock": 0},
-           {"id": "mk4", "name": "áo cây dừa", "color": "vàng", "stock": 0},
+product = [{"id": "mk1", "name": "áo trái tim", "color": "xanh", "stock": 0, "price": "150 000"},
+           {"id": "mk1", "name": "áo trái tim", "color": "đỏ", "stock": 2, "price": "150 000"},
+           {"id": "mk1", "name": "áo trái tim", "color": "tím", "stock": 2, "price": "150 000"},
+           {"id": "mk1", "name": "áo trái tim", "color": "vàng", "stock": 2, "price": "150 000"},
+           {"id": "mk2", "name": "áo cô gái", "color": "xanh", "stock": 2, "price": "150 000"},
+           {"id": "mk2", "name": "áo cô gái", "color": "đỏ", "stock": 2, "price": "150 000"},
+           {"id": "mk2", "name": "áo cô gái", "color": "tím", "stock": 2, "price": "150 000"},
+           {"id": "mk2", "name": "áo cô gái", "color": "vàng", "stock": 2, "price": "150 000"},
+           {"id": "mk3", "name": "áo bọt biển", "color": "xanh", "stock": 2, "price": "150 000"},
+           {"id": "mk3", "name": "áo bọt biển", "color": "đỏ", "stock": 0, "price": "150 000"},
+           {"id": "mk3", "name": "áo bọt biển", "color": "tím", "stock": 0, "price": "150 000"},
+           {"id": "mk3", "name": "áo bọt biển", "color": "vàng", "stock": 0, "price": "150 000"},
+           {"id": "mk4", "name": "áo cây dừa", "color": "xanh", "stock": 9, "price": "150 000"},
+           {"id": "mk4", "name": "áo cây dừa", "color": "đỏ", "stock": 7, "price": "150 000"},
+           {"id": "mk4", "name": "áo cây dừa", "color": "tím", "stock": 0, "price": "150 000"},
+           {"id": "mk4", "name": "áo cây dừa", "color": "vàng", "stock": 0, "price": "150 000"},
            ]
 def product_avail(id, color):
     for p in product:
@@ -75,6 +75,31 @@ class ActionColorAsk(Action):
                     mess = "Bên em còn màu " + color + " nhé ạ!"
                 else:
                     mess = "Bên em hiện tại đang hết màu " + color +", anh/chị có thể cân nhắc màu khác ạ!"
+
+        dispatcher.utter_message(text=mess)
+        return []
+
+
+class ActionPrice(Action):
+
+    def name(self) -> Text:
+        return "action_price"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message["entities"]
+        print(entities)
+
+        mess = "Giá áo bên shop rẻ nhất thị trường chỉ từ 100k đến 300k. Anh chị muốn biết giá cụ thể cho từng sản phẩm vui lòng cho shop mã sản phẩm nhé ạ!"
+        if entities != None:
+            for e in entities:
+                if e["entity"] == "product_id":
+                    prod_id = e["value"]
+                    for p in product:
+                        if p["id"] == prod_id:
+                            mess = p["name"] + "-" + p["id"] + " bên em đang bán chỉ " + p["price"]
+                    
 
         dispatcher.utter_message(text=mess)
         return []
